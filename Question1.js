3e302865b7cfc5cbab27be42601b3600055dc8bb
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-09-17 22:55:14 -0300

    ignor case

--- Question1.js
+++ Question1.js
@@ -3,9 +3,16 @@ Author: Shaghayegh Amouhossein
 Date: September 14th 2023
 Title: Question 1
 */
+
+// creating function
 function func(inputString) 
 {
+    // Convert the string to lower case to ignor case
+    const lowerCaseString = inputString.toLowerCase();
+  
     
+
+
 }
   
 
