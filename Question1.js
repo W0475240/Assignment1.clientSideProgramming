1ed96ff6e225f9e4005af50d4de6f7bb2074af15
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-09-18 00:05:37 -0300

    function for removin letters

--- Question1.js
+++ Question1.js
@@ -7,11 +7,25 @@ Title: Question 1
 // creating function
 function func(inputString) 
 {
-    // Convert the string to lower case to ignor case
+    // Convert the string to lower case for case-insensitive comparison
     const lowerCaseString = inputString.toLowerCase();
-  
-    
 
+    // if the first and last characters of the input string are the same 
+    if (lowerCaseString[0] === lowerCaseString[inputString.length - 1]) 
+    { 
+      
+      // If they are the same, reverse the string and return it
+      return inputString.split('').reverse().join('');
+    }
+     
+    else 
+    {  
+      // If they are different, delet the first and last charachter
+      // return the modified string
+      return inputString.slice(1, -1);
+    }
+    
+ 
 
 }
   
