49d2800a99c6e7df08948e63d1cbdaacaf4ee930
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-09-19 14:51:59 -0300

    adding array and creating function

--- Question2.js
+++ Question2.js
@@ -3,3 +3,18 @@ Author: Shaghayegh Amouhossein
 Date: September 14th 2023
 Title: Question 2
 */
+
+// creating function for storying data 
+function func(arr) 
+{
+  // Initialize an array to store consecutive increasing numbers within
+  var consecutives_arr = [[]];
+  var j = 0; // Initialize an index for the consecutive sequences array
+  var flag = false; // Initialize a flag to to gather consecutive elements
+
+ 
+  }
+
+
+
+
