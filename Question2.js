924e5539ac39481c7208cb37455e4e307bbaffe1
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-09-19 23:45:23 -0300

    loop

--- Question2.js
+++ Question2.js
@@ -10,11 +10,29 @@ function func(arr)
   // Initialize an array to store consecutive increasing numbers within
   var consecutives_arr = [[]];
   var j = 0; // Initialize an index for the consecutive sequences array
-  var flag = false; // Initialize a flag to to gather consecutive elements
+  var flag = false; // Initialize a flag to track consecutive elements
 
  
   }
-
+  // Loop through the input array
+  for(var i=0;i<arr.length;i++)
+  {
+    // if the element is consecutive to the previous element
+    if( ( i == arr.length-1 && arr[i] == arr[i-1]+1 ) || arr[i] == arr[i+1]-1)
+    {
+      // push the element to the array and set the flag to true
+      var flag = true
+      consecutives_arr[j].push(arr[i]);
+    }
+    else if(flag == true)
+    {
+      // if the flag is true, push the element to the array and increment the array index
+      consecutives_arr[j].push(arr[i]);
+      j++;
+      consecutives_arr[j] = []; // Create a new empty array for the next sequence
+      flag = false; // Reset the flag 
+    }
+  }
 
 
 
