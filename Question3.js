984f834f3b9d1c235ee3f21aa33a7a8f90be5714
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-09-20 09:16:31 -0300

    calculating and output

--- Question3.js
+++ Question3.js
@@ -5,15 +5,25 @@ Title: Question 3
 */
 
 // Define the date 
-const myNextBirthday = new Date(2024, 5, 15, 0, 0, 0);
+const myNextBirthday = new Date(2024, 1, 5, 0, 0, 0);
 
-// Get the current date and time
+// write current date and time
 const currentDate = new Date();
 
 // Calculate the time difference
-const timeDifference = myNextBirthday - currentDate;
+const differenceDate = myNextBirthday - currentDate;
 
 // Calculate the remaining time 
-
+// seconds
+const SecRemain = Math.floor(differenceDate / 1000) % 60;
+// minutes
+const minRemain = Math.floor((differenceDate / (1000 * 60)) % 60);
+// hour
+const hoursRemain = Math.floor((differenceDate / (1000 * 60 * 60)) % 24);
+// day
+const daysRemain = Math.floor((differenceDate / (1000 * 60 * 60 * 24)) % 7);
+// week
+const weeksRemain = Math.floor(differenceDate / (1000 * 60 * 60 * 24 * 7));
 
 // Output the remaining time 
+console.log("there are " + weeksRemain + " weeks, " + daysRemain + " days, " + hoursRemain + " hours, " + minRemain + " minutes, and " + SecRemain + " seconds left until my next birthday");
